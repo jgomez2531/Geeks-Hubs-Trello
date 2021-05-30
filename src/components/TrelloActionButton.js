2f0fd9b19d81076b9ed5607cee381b166ca56dca
jgomez2531 <76822966+jgomez2531@users.noreply.github.com>
2021-05-30 13:25:12 +0200

    30/05/2021
    Añadir botones de agregar nueva tarjeta a una lista existente y para añadir una nueva lista (No funcionalidades).

---
+++ src/components/TrelloActionButton.js
@@ -0,0 +1,125 @@
+import React from "react";
+import Icon from "@material-ui/core/Icon";
+import TextArea from "react-textarea-autosize";
+import Card from '@material-ui/core/Card';
+import { Button } from "@material-ui/core";
+
+class TrelloActionButton extends React.Component {
+
+    state = {
+        formOpen: false
+    };
+
+    openForm = () => {
+        this.setState({
+            formOpen: true
+        });
+    }
+
+    closeForm = (e) => {
+        this.setState({
+            formOpen: false
+        });
+    }
+
+    handleInputChange = e => {
+        this.setState({
+            text: e.target.value
+        });
+    };
+
+    renderAddButton = () => {
+        const {list} = this.props;
+
+        const buttonText = list ? "Añadir otra lista" : "Añadir otra tarjeta";
+        const buttonTextOpacity = list ? 1 : 0.5;
+        const buttonTextColor = list ? "white" : "inherit";
+        const buttonTextBackground = list ? "rgba(0,0,0,.15)" : "inherit";
+
+        return (
+            <div
+                onClick={this.openForm}
+                style={{
+                    ...styles.openFormButtonGroup,
+                    opacity: buttonTextOpacity,
+                    color: buttonTextColor,
+                    backgroundColor: buttonTextBackground
+                }}
+            >
+                <Icon>add</Icon>
+                <p>{buttonText}</p>
+            </div>
+        );
+
+    };
+
+    renderForm = () => {
+        const {list} = this.props;
+
+        const placeholder = list
+            ? "Introduzca un título para la lista ..."
+            : "Introduzca un título para la tarjeta...";
+        
+        const buttonTitle = list ? "Añadir Lista" : "Añadir Tarjeta";
+
+        return <div>
+            <Card
+                style={{
+                    minHeight: 85,
+                    minWidth: 272,
+                    padding: "6px 8px 2px"
+                }}
+            >
+                <TextArea 
+                    placeholder={placeholder} 
+                    autoFocus 
+                    onBlur={this.closeForm}
+                    onChange={this.handleInputChange}
+                    style={{
+                        resize: "none",
+                        width: "100%",
+                        overflow: "hidden",
+                        outline: "none",
+                        border: "none"
+                    }}
+                />
+            </Card>
+            <div style={styles.formButtonGroup}>
+                <Button 
+                    variant="contained"
+                    style={{
+                        color: "white",
+                        backgroundColor: "#5aac44"
+                    }} 
+                >{buttonTitle}{" "}</Button>
+                <Icon style={{marginLeft: 8, cursor: "pointer"}}>close</Icon>
+            </div>
+        </div>
+    };
+
+    render() {
+        return this.state.formOpen ? this.renderForm() : this.renderAddButton();
+    }
+
+}
+
+const styles = {
+
+    openFormButtonGroup: {
+        display: "flex",
+        alignItems: "center",
+        cursor: "pointer",
+        boderRadius: 3,
+        height: 36,
+        width: 272,
+        paddingLeft: 10
+    },
+
+    formButtonGroup: {
+        marginTop: 8,
+        display: "flex",
+        alignItems: "center"
+    }
+};
+
+export default TrelloActionButton;
--- src/components/TrelloList.js
+++ src/components/TrelloList.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import TrelloCard from './TrelloCard';
+import TrelloActionButton from "./TrelloActionButton";
 
 const TrelloList = ({title, cards}) => {
     return(
         <div style={styles.container}>
             <h4>{title}</h4>
             {cards.map(card => (
-                <TrelloCard  text={card.text} />
+                <TrelloCard  key={cards.id} text={card.text} />
             ))}
+            <TrelloActionButton />
         </div>
     )
 }
@@ -15,13 +17,11 @@ const TrelloList = ({title, cards}) => {
 const styles = {
     container: {
         background: "#dfe3e6",
-        bborderBottomLeftRadius: 3,
-        borderBottomRightRadius: 3,
-        borderTopLeftRadius: 3,
-        borderTopRightRadius: 3,
+        boderRadius: 3,
         width: 300,
         padding: 8,
-        marginRight: 8
+        marginRight: 8,
+        height: "100%"
     }
 }
 export default TrelloList;
